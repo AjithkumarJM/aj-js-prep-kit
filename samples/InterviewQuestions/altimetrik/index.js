b7d7a1eef26c9d04ba1bbfaaa36b4127ab843acb
Ajithkumar Juttun Moorthy <ajithkumar.moorthy@freshworks.com>
2024-07-30 13:16:44 +0530

    Adds altimetrik interview questions

---
+++ samples/InterviewQuestions/altimetrik/index.js
@@ -0,0 +1,11 @@
+// round 1
+// Design a app with react/redux/node and implement jwt enabled authorization
+
+// round 2
+// Tech discussion about typescript, reconciliation, current proejcts, how do you enhancement of an app
+
+// round 3
+// SSR v CSR, AWS services, how do you enhancement of an app, git fetch vs pull
+
+// round 4
+// react data afetch
